a72963d2ed77c66370912f03a2d7c670ccef6877
alex0003alex <75651985+alex0003alex@users.noreply.github.com>
2023-04-08 19:50:36 +0800

    Update customize.sh

--- customize.sh
+++ customize.sh
@@ -5,5 +5,7 @@
 # Author: eSirPlayground
 # Youtube Channel: https://goo.gl/fvkdwm 
 #=================================================
+git clone https://github.com/destan19/OpenAppFilter.git package/OpenAppFilter
+
 #1. Modify default IP
-sed -i 's/192.168.1.1/192.168.5.1/g' openwrt/package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.1.2/g' openwrt/package/base-files/files/bin/config_generate
